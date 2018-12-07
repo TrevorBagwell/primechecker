b48cc5ec46da846b933995ecdd4d20440e0adbdb
TrevorBagwell <tbagwel@purdue.edu>
2018-12-07 12:17:37 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,27 +1,5 @@
-const express = require('express');
-const app = express();
-const port = 2500;
-const path = require('path');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/expressdemo');
-
-const CoinRouter = require('./routes/CoinRouter');
-
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-app.use('/coins', CoinRouter);
-
-app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname,'public', 'index.html'));
-});
-
-app.listen(port, function(){
-  console.log('Node js Express js Tutorial at port', port);
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
 });
